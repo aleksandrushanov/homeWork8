0db31f5277ee00a94d4bb99ba31936f776a41823
Саша <kresh_sasha@icloud.com>
2022-11-11 23:41:25 +0700

    task3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашнее задание");
@@ -6,38 +9,14 @@ public class Main {
         number[0] = 1;
         number[1] = 2;
         number[2] = 3;
-        for (int i = number.length - 1; i >=0; i--) {
-            if (i!=0){
-                System.out.print(number[i]+" ,");
-            }else {
-                System.out.print(number[i]);
-            }
-        }
-        System.out.println("\n");
+        for (int i = 0; i <number.length; i++) {
 
-        double[] fractional ={1.57, 7.654, 9.986};
-        for (int a = fractional.length - 1 ;a >=0; a--) {
-            if (a !=0){
-                System.out.print(fractional[a] + ", ");
-            }else {
-                System.out.print(fractional[a]);
+            if ( number[i] % 2 !=0) {
+                number[i]++;
             }
         }
+        System.out.println(Arrays.toString(number));
 
-        System.out.println("\n");
-        int [] age = new int[4];
-        age[0]=15;
-        age[1]=16;
-        age[2]=17;
-        age[3]=18;
-        for (int i =age.length - 1; i >=0; i--) {
-            if (i !=0){
-                System.out.print(age[i] +", ");
-            }else {
-                System.out.print(age[i]);
-            }
-            
-        }
 
 
     }
