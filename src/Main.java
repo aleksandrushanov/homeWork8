e7f63e67191c19970baefd0e19a6ad07d182dc55
Саша <kresh_sasha@icloud.com>
2022-11-11 22:43:54 +0700

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Домашнее задание");
+        // Задание 1
+        int[] number = new int[3];
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
+        for (int i = 0; i < 3; i++) {
+            System.out.print(number[2]);
+        }
+        System.out.println("\n");
+
+        double[] fractional ={1.57, 7.654, 9.986};
+        System.out.println(fractional[2]);
+
+        System.out.println("\n");
+        int [] age = new int[4];
+        age[0]=15;
+        age[1]=16;
+        age[2]=17;
+        age[3]=18;
+        System.out.println(age[2]);
+
+
     }
 }
